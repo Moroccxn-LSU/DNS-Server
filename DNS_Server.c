5df7a7fe1586fec6e0eeb71d3515747dc4ceab6e
Adam Elkhanoufi <103407697+Moroccxn-LSU@users.noreply.github.com>
2023-03-05 18:43:49 -0600

    Update DNS_Server.c

--- DNS_Server.c
+++ DNS_Server.c
@@ -2,16 +2,8 @@
     Adam Elkhanoufi
     Simple DNS Server in C
     12/02/2022
-
-    Used socket programming libraries to create a program that opens up a
-    port on your machine where a max of 10 "clients" can make a DNS request.
-
-    The program then gets the DNS query, resolves the Domain name, and responds accordingly to the "client"
-
-    Program runs and accepts DNS queries until this program is terminated
-
-    NOTE: Make sure port 53 on your machine is available 
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
